641957ac349f042a86bf67845d999fb6fcc28505
Afrahexotel <afrah.mahmud@exotel.com>
2025-09-15 17:28:58 +0530

    phone.js

--- demo-non-npm/phone.js
+++ demo-non-npm/phone.js
@@ -1,16 +1,16 @@
 phone = '[\
-{\
-    "Username":"vijayk83e4b9dd",\
-    "DisplayName":"Guru",\
-    "HostServer":"voip.qaexotel.com",\
-    "Domain":"exotel41us3.voip.exotel.com",\
-    "Port":443,\
-    "Password":"abcd12345",\
-    "CallTimeout":1000,\
-    "Security": "wss",\
-    "EndPoint": "wss",\
-    "AccountSID":"exotelt1",\
-    "AccountNo":"ashishrb544b97d",\
-    "AutoRegistration": true\
-}\
+    {\
+        "Username":"vijayk83e4b9dd",\
+        "DisplayName":"Guru",\
+        "HostServer":"voip.qaexotel.com",\
+        "Domain":"exotel41us3.voip.exotel.com",\
+        "Port":443,\
+        "Password":"abcd12345",\
+        "CallTimeout":1000,\
+        "Security": "wss",\
+        "EndPoint": "wss",\
+        "AccountSID":"exotelt1",\
+        "AccountNo":"ashishrb544b97d",\
+        "AutoRegistration": true\
+    }\
 ]';
