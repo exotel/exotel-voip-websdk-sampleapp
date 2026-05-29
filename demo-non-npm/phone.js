58ce2150efec46f4569092fde8de1ac906e7b268
vijay.kumar <vijaykumar@exotel.com>
2026-05-29 23:40:48 +0530

    Revert "Remove customer-specific SIP credentials and account IDs from sample app"
    This reverts commit 9f8949bf96ff6527dc7703be5690394d9487eb1f.

--- demo-non-npm/phone.js
+++ demo-non-npm/phone.js
@@ -1,16 +1,17 @@
+// Success-criteria SIP account (3.0.12 regression): fe2f6aa4f1f19da524946af2032b1a5t
 phone = '[\
     {\
-        "Username":"<VOIP Username>",\
-        "DisplayName":"<Display Name>",\
-        "HostServer":"<VOIP Proxy Address>",\
-        "Domain":"<VOIP Domain Address>",\
+        "Username":"vijayk0aa794fd",\
+        "DisplayName":"Guru",\
+        "HostServer":"in.voip.exotel.com",\
+        "Domain":"siptrunkingpoc1m.voip.exotel.com",\
         "Port":443,\
-        "Password":"<VOIP Password>",\
+        "Password":"abcd1234",\
         "CallTimeout":1000,\
         "Security": "wss",\
         "EndPoint": "wss",\
-        "AccountSID":"<Your Account SID>",\
-        "AccountNo":"<VOIP Username>",\
+        "AccountSID":"exotelt1",\
+        "AccountNo":"ashishrb544b97d",\
         "AutoRegistration": true\
     }\
 ]';
